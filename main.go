87d9e14724d1e05388a195c87ec5d98a6f9e18d2
Hamza Çağrıcı <cagrici24@itu.edu.tr>
2026-08-25 18:27:47 +0300

    chore: bump version to 0.9.0

--- main.go
+++ main.go
@@ -35,7 +35,7 @@ import (
 )
 
 const (
-	onyxVersion     = "0.8.0"
+	onyxVersion     = "0.9.0"
 	defaultDB       = "data/wordfence.json"
 	feedProduction  = "production"
 	feedScanner     = "scanner"
